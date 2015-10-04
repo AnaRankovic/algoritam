e9ac09b2d8b6d35efda7d0f91794e0d17f8acf0d
AnaRankovic <anarankovic11@gmail.com>
2015-10-04 10:44:15 +0200

    Created data base Added formula for Euclidean distance

---
+++ algoritam/src/algoritam/algoritam.clj
@@ -0,0 +1,6 @@
+(ns algoritam.algoritam)
+
+(defn Euclidean [x1 x2 y1 y2]
+     (Math/sqrt( + (Math/pow 2 (- x1 x2)) (Math/pow 2 (- y1 y2)))))
+
+
---
+++ algoritam/src/algoritam/models/db.clj
@@ -0,0 +1,25 @@
+(ns algoritam.models.db
+     (:require [clojure.java.jdbc :as sql])
+    (:import java.sql.DriverManager))
+
+(def db
+  "Definition of database"
+  {:classname "org.sqlite.JDBC",
+         :subprotocol "sqlite",
+         :subname "film_critics.sqlite"})
+
+(defn create-table []
+    "Creates table"
+  (sql/with-connection
+    db
+    (sql/create-table
+      :film_critics
+      [:Id "INTEGER PRIMARY KEY AUTOINCREMENT"]
+      [:Name "TEXT"]
+      [:Lady_in_the_Water "REAL"]
+      [:Snakes_on_a_Plane "REAL"]
+      [:Just_My_Luck "REAL"]
+      [:Superman_Returns "REAL"]
+      [:You_Me_and_Dupree "REAL"]
+      [:The_Night_Listener"REAL"]
+      )))
