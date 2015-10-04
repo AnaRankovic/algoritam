6c500769e372039e1ce5bf106f8831395158445a
AnaRankovic <anarankovic11@gmail.com>
2015-10-05 01:36:39 +0200

    Euklid gotov

--- algoritam/src/algoritam/algoritam.clj
+++ algoritam/src/algoritam/algoritam.clj
@@ -1,28 +1,17 @@
-(ns algoritam.algoritam)
+(ns algoritam.algoritam
+    (:require [algoritam.models.db :as db]))
 
-(defn Euclidean [x1 x2 y1 y2]
-     (/ 1 (+ 1(Math/sqrt( + (Math/pow 2 (- x1 x2)) (Math/pow 2 (- y1 y2)))))))
+(def suma (atom 0.0))
 
-//napravi funkciju kojoj prosledjujes 2 imena, a on ti vraca njihove skorove
-//prolazi kroz sve filmove i vraca euklida
-//Euklidu prosledjujes imena izvodjaca
-//vrednosti sumiras i na kraju radis 1/(1 + suma)
-//Ako nemaju zajednicke rejtinge vracas 0 -->  ako je neki null
-//pravi metode za svaki red u tabeli
-
-
-from math import sqrt
-# Returns a distance-based similarity score for person1 and person2
-def sim_distance(prefs,person1,person2):
-# Get the list of shared_items
-si={}
-for item in prefs[person1]:
-if item in prefs[person2]:
-si[item]=1
-# if they have no ratings in common, return 0
-if len(si)==0: return 0
-# Add up the squares of all the differences
-sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
-for item in prefs[person1] if item in prefs[person2]])
-return 1/(1+sum_of_squares)
-This function can be called with two
+(defn sumaKvadrataRazlike [x1 x2]
+  (if (and (not= nil x1) (not= nil x2))      
+     (swap! suma (fn [n] (+ n (Math/pow (- x1 x2) 2))))))
+     
+(defn euklid [kriticar1, kriticar2]
+  (sumaKvadrataRazlike (algoritam.models.db/vrati-Lady_in_the_Water kriticar1) (algoritam.models.db/vrati-Lady_in_the_Water kriticar2))
+  (sumaKvadrataRazlike (algoritam.models.db/vrati-Snakes_on_a_Plane kriticar1) (algoritam.models.db/vrati-Snakes_on_a_Plane kriticar2))
+  (sumaKvadrataRazlike (algoritam.models.db/vrati-Just_My_Luck kriticar1) (algoritam.models.db/vrati-Just_My_Luck kriticar2))
+  (sumaKvadrataRazlike (algoritam.models.db/vrati-Superman_Returns kriticar1) (algoritam.models.db/vrati-Superman_Returns kriticar2))
+  (sumaKvadrataRazlike (algoritam.models.db/vrati-You_Me_and_Dupree kriticar1) (algoritam.models.db/vrati-You_Me_and_Dupree kriticar2))
+  (sumaKvadrataRazlike (algoritam.models.db/vrati-The_Night_Listener kriticar1) (algoritam.models.db/vrati-The_Night_Listener kriticar2))
+  (/ 1 (+ 1 (Math/sqrt @suma))))
