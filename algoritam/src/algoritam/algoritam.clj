7f9baea7b368ca6dd4c6f248b875a447c13ad4b6
AnaRankovic <anarankovic11@gmail.com>
2015-10-04 13:53:27 +0200

    Dodate funkcije koje vracaju ocene za sve filmove u bazi, korisnik unosi ime filmskog kriticara

--- algoritam/src/algoritam/algoritam.clj
+++ algoritam/src/algoritam/algoritam.clj
@@ -2,3 +2,27 @@
 
 (defn Euclidean [x1 x2 y1 y2]
      (/ 1 (+ 1(Math/sqrt( + (Math/pow 2 (- x1 x2)) (Math/pow 2 (- y1 y2)))))))
+
+//napravi funkciju kojoj prosledjujes 2 imena, a on ti vraca njihove skorove
+//prolazi kroz sve filmove i vraca euklida
+//Euklidu prosledjujes imena izvodjaca
+//vrednosti sumiras i na kraju radis 1/(1 + suma)
+//Ako nemaju zajednicke rejtinge vracas 0 -->  ako je neki null
+//pravi metode za svaki red u tabeli
+
+
+from math import sqrt
+# Returns a distance-based similarity score for person1 and person2
+def sim_distance(prefs,person1,person2):
+# Get the list of shared_items
+si={}
+for item in prefs[person1]:
+if item in prefs[person2]:
+si[item]=1
+# if they have no ratings in common, return 0
+if len(si)==0: return 0
+# Add up the squares of all the differences
+sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
+for item in prefs[person1] if item in prefs[person2]])
+return 1/(1+sum_of_squares)
+This function can be called with two
--- algoritam/src/algoritam/models/db.clj
+++ algoritam/src/algoritam/models/db.clj
@@ -21,5 +21,46 @@
       [:Just_My_Luck "REAL"]
       [:Superman_Returns "REAL"]
       [:You_Me_and_Dupree "REAL"]
-      [:The_Night_Listener"REAL"]
-      )))
+      [:The_Night_Listener "REAL"])))
+
+(defn vrati-Lady_in_the_Water [Name]
+  "Select user with given name"
+  (sql/with-connection
+  db
+   (get (sql/with-query-results res
+    ["select Lady_in_the_Water from film_critics where Name = ?" Name] (first res)) :lady_in_the_water)))
+
+(defn vrati-Snakes_on_a_Plane [Name]
+  "Select user with given name"
+  (sql/with-connection
+  db
+   (get (sql/with-query-results res
+    ["select Snakes_on_a_Plane from film_critics where Name = ?" Name] (first res)) :snakes_on_a_plane)))
+
+(defn vrati-Just_My_Luck [Name]
+  "Select user with given name"
+  (sql/with-connection
+  db
+  (get (sql/with-query-results res
+    ["select Just_My_Luck from film_critics where Name = ?" Name] (first res)) :just_my_luck)))
+
+(defn vrati-Superman_Returns [Name]
+  "Select user with given name"
+  (sql/with-connection
+  db
+   (get (sql/with-query-results res
+    ["select Superman_Returns from film_critics where Name = ?" Name] (first res)) :superman_returns )))
+
+(defn vrati-You_Me_and_Dupree [Name]
+  "Select user with given name"
+  (sql/with-connection
+  db
+   (get (sql/with-query-results res
+    ["select You_Me_and_Dupree from film_critics where Name = ?" Name] (first res)) :you_me_and_dupree)))
+
+(defn vrati-The_Night_Listener [Name]
+  "Select user with given name"
+  (sql/with-connection
+  db
+   (get (sql/with-query-results res
+    ["select The_Night_Listener from film_critics where Name = ?" Name] (first res)) :the_night_listener)))
