dfd1da8096abfab443cd0fef52ebc26881d7b0ec
AnaRankovic <anarankovic11@gmail.com>
2015-10-04 11:25:48 +0200

    Euclidean - normalizacija: [0 1]

--- algoritam/src/algoritam/algoritam.clj
+++ algoritam/src/algoritam/algoritam.clj
@@ -1,6 +1,4 @@
 (ns algoritam.algoritam)
 
 (defn Euclidean [x1 x2 y1 y2]
-     (Math/sqrt( + (Math/pow 2 (- x1 x2)) (Math/pow 2 (- y1 y2)))))
-
-
+     (/ 1 (+ 1(Math/sqrt( + (Math/pow 2 (- x1 x2)) (Math/pow 2 (- y1 y2)))))))
